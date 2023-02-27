81423b52f86ec3be0c628762ec1c55a4416dbde1
alexher <alexhernandez.pro@gmail.com>
2023-02-27 14:22:45 +0100

    ajout vérification message vide et voir plus

--- src/components/HomePage/Chat/ChatList/ChatList.tsx
+++ src/components/HomePage/Chat/ChatList/ChatList.tsx
@@ -1,5 +1,5 @@
-import { useRef } from 'react';
-import Spinner from '../../../Spinner';
+import { useRef, useState } from 'react'
+import Spinner from '../../../Spinner'
 
 export interface Message {
     img: string
@@ -8,22 +8,37 @@ export interface Message {
     dateCreated: string
 }
 interface ChatListProps {
-    error: string;
-    getMoreMessages: () => void;
-    loading: boolean;
-    listMessages: Message[];
-    username: string;
+    error: string
+    getMoreMessages: () => void
+    loading: boolean
+    listMessages: Message[]
+    username: string
 }
 
 export default function ChatList(props: ChatListProps) {
-
     const parentDivRef = useRef<HTMLDivElement>(null)
 
     const handleScroll = () => {
         // Check if the user has scrolled to the top of the div
         if (parentDivRef.current!.scrollTop === 0) {
-            props.getMoreMessages();
+            props.getMoreMessages()
         }
+    }
+
+    const [showFullMessage, setShowFullMessage] = useState<{[key: number]: boolean}>({})
+
+    const MAX_MESSAGE_LENGTH = 100;
+
+    const handleMessageDisplay = (message:string, i:number) => {
+        if (message.length <= MAX_MESSAGE_LENGTH || showFullMessage[i]) {
+            return message.trim()
+        } else {
+            return message.slice(0, MAX_MESSAGE_LENGTH).trim() + '...';
+        }
+    };
+
+    const handleShowFullMessage = (i:number) => {
+        setShowFullMessage({...showFullMessage, [i]: true})
     }
 
     return (
@@ -36,11 +51,15 @@ export default function ChatList(props: ChatListProps) {
                         <p>{msg.pseudo}</p>
                     </div>
                     <div
-                        className={`border p-2 pr-4 mb-1 rounded-3xl text-white flex items-center ${props.username === msg.pseudo ? 'bg-black' : 'bg-[#50248d]'
-                            }`}
+                        className={`border p-2 pr-4 mb-1 rounded-3xl text-white flex items-center ${
+                            props.username === msg.pseudo ? 'bg-black' : 'bg-[#50248d]'
+                        }`}
                     >
                         <img className="rounded-full h-7 w-7 mr-2" src={`img/${msg.img}`} alt="profil" />
-                        <pre className="whitespace-pre-wrap">{msg.message}</pre>
+                        <pre className="whitespace-pre-wrap">{handleMessageDisplay(msg.message, i)}</pre>
+                        {msg.message.length > MAX_MESSAGE_LENGTH && !showFullMessage[i] &&
+                            <button className='ml-2 hover:underline text-blue-400 focus:outline-none' onClick={() => handleShowFullMessage(i)}>Voir plus</button>
+                        }
                     </div>
                 </div>
             ))}
